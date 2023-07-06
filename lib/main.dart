1ebe0520a6750b273208ca0d204655a8cf365518
amalnaroth <amalzenox129@gmail.com>
2023-07-06 19:45:43 +0530

    animation added

--- lib/main.dart
+++ lib/main.dart
@@ -9,48 +9,64 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Animated Login Page",
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        brightness: Brightness.dark
-      ),
-      home:  LoginScreen(),
+      theme: ThemeData(primarySwatch: Colors.grey, brightness: Brightness.dark),
+      home: LoginScreen(),
     );
   }
 }
 
-class LoginScreen extends StatefulWidget{
-   LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStateMixin{
-  AnimationController? iconAnimationController;
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _iconAnimationController;
 
-  Animation<double>? animation;
+  Animation<double>? _iconAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _iconAnimation = CurvedAnimation(
+        parent: _iconAnimationController, curve: Curves.easeOut);
+    _iconAnimation!.addListener(
+      () => this.setState(() {}),
+    );
+    _iconAnimationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: Colors.red.withOpacity(0.5),
-      body:const Stack(
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
+      body: Stack(
         fit: StackFit.expand,
         children: [
-        Image(image: AssetImage("assets/wallpaperflare.com_wallpaper (1).jpg"),
-        fit: BoxFit.cover,
-        color: Colors.black,),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-          FlutterLogo(
-            size: 100,
+          const Image(
+            image: AssetImage("assets/wallpaperflare.com_wallpaper (1).jpg"),
+            fit: BoxFit.cover,
+            color: Colors.black,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlutterLogo(
+                size: _iconAnimation!.value * 100,
+              ),
+              //Form(child: )
+            ],
           )
-        ],)
-      ],),
+        ],
+      ),
     );
   }
 }
